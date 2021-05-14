b05f79ab5ad8426b07843640ab3d69ab0dce4775
150149 <1802796278@qq.com>
2021-05-14 23:33:43 +0800

    2021.5.14
    新增 AAC NEW变种防御

--- src/ciyuanwutuobang/Crashbye.java
+++ src/ciyuanwutuobang/Crashbye.java
@@ -15,20 +15,26 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Crashbye  extends JavaPlugin implements Listener {
     private ProtocolManager pm;
     Plugin thisplug = this;
+    private Map<String, Double> LastX = new HashMap<>();
+    private Map<String,Integer> KickCount = new HashMap<>();
+    private Map<String,Integer> CheckTimer = new HashMap<>();
 
     @Override
     public void onEnable(){
         Bukkit.getPluginManager().registerEvents(this, this);
-        Bukkit.getConsoleSender().sendMessage("防蹦服已开启");
+        Bukkit.getConsoleSender().sendMessage("防蹦服2.0已开启");
         Bukkit.getConsoleSender().sendMessage("作者：150149 QQ：1802796278");
         pm = ProtocolLibrary.getProtocolManager();
 
         pm.addPacketListener(new PacketAdapter(PacketAdapter.params()
 
-                .plugin(this)
+                .plugin(thisplug)
                 .clientSide()
                 .listenerPriority(ListenerPriority.HIGH)
                 .gamePhase(GamePhase.PLAYING)
@@ -37,6 +43,9 @@ public class Crashbye  extends JavaPlugin implements Listener {
         ) {
             @Override
             public void onPacketReceiving(PacketEvent event) {
+                if (event.getPlayer().getName().contains("LOTR")) {
+                    return;
+                }
                 PacketContainer packet = event.getPacket();
                 PacketType packettype = event.getPacketType();
 
@@ -44,7 +53,12 @@ public class Crashbye  extends JavaPlugin implements Listener {
                 double y = packet.getDoubles().getValues().get(1);
                 double z = packet.getDoubles().getValues().get(2);
 
-                if (y>3000 || x > 1000000 || z > 1000000) {
+
+                LastX.putIfAbsent(event.getPlayer().getName(),x);
+                KickCount.putIfAbsent(event.getPlayer().getName(),0);
+                CheckTimer.putIfAbsent(event.getPlayer().getName(),0);
+
+                if (y>1000 || x>1000000 || y>1000000) {
                     event.setCancelled(true);
                     BukkitTask time=new BukkitRunnable() {
                         @Override
@@ -55,14 +69,37 @@ public class Crashbye  extends JavaPlugin implements Listener {
                             this.cancel();
                         }
                     }.runTaskTimer(thisplug, 2L,0);
+                } else if ( Math.abs(x- LastX.get(event.getPlayer().getName()))>5000) {
+                    KickCount.put(event.getPlayer().getName(),KickCount.get(event.getPlayer().getName())+ 1);
+                    CheckTimer.put(event.getPlayer().getName(),30);
                 }
+
+                if (CheckTimer.get(event.getPlayer().getName())>0) {
+                    CheckTimer.put(event.getPlayer().getName(),CheckTimer.get(event.getPlayer().getName())-1);
+                }
+                if (CheckTimer.get(event.getPlayer().getName())<=0) {
+                    KickCount.put(event.getPlayer().getName(),0);
+                }
+                if (KickCount.get(event.getPlayer().getName())>=8) {
+                    BukkitTask time=new BukkitRunnable() {
+                        @Override
+                        public void run() {
+                            if (!event.getPlayer().isBanned()) {
+                                Bukkit.getServer().dispatchCommand(Bukkit.getConsoleSender(),"ban " + event.getPlayer().getName() +" &7[ &c反作弊 &7]&f禁止崩服[AAC new]");
+                            }
+                            KickCount.put(event.getPlayer().getName(),0);
+                            this.cancel();
+                        }
+                    }.runTaskTimer(thisplug, 2L,0);
+                }
+                LastX.put(event.getPlayer().getName(),x);
             }
         });
     }
 
     @Override
     public void onDisable() {
-        Bukkit.getConsoleSender().sendMessage("防蹦服已关闭");
+        Bukkit.getConsoleSender().sendMessage("防蹦服2.0已关闭");
     }
 
 }
